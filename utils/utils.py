123c86e1e63694b84215dfc554dd1e098336a0ef
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2024-05-08 22:44:12 +0200

    Revert "prova aggiornamento dati"
    This reverts commit 074b7b40af59eae22cd7cc86d0edda170f30d132.

--- utils/utils.py
+++ utils/utils.py
@@ -1,37 +1,6 @@
-from turtle import st
-import pandas as pd
 import toml
 import streamlit_authenticator as stauth
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from utils.constants import column_mapping, weeks, new_columns
-import json
 
-
-@st.cache_data
-def read_google_sheet():
-    print('Leggi dati')
-    # Define the scope of the Google Sheet
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-
-    # Load credentials from the TOML file
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["google_secret"].to_dict(), scope)
-
-    # Authenticate using the credentials
-    gc = gspread.authorize(credentials)
-
-    # Open the Google Sheet by its title
-    sheet = gc.open('Iscrizioni centro estivo (Risposte)').sheet1
-
-    # Read all data from the sheet
-    data = sheet.get_all_records()
-
-    df = pd.DataFrame(data)
-    df.columns = new_columns
-
-    df = st.session_state['data']
-
-    return df
 
 #--------------------------------------
 #--------- AUTHENTICATION -------------
